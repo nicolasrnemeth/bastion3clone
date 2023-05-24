2255db716c6c0df2d644ca35faf0c925df46604b
Nicolas Robert Nemeth <56492973+nicolasrnemeth@users.noreply.github.com>
2023-05-24 13:04:54 +0200

    Delete build/lib/src directory

--- build/lib/src/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-name = "Bastion3Clone"
-__version__ = "1.0.0"
-__all__ = ["encoders", "sequtils", "__version__", "predictor", "training"]
-_ROOT = os.path.abspath(os.path.dirname(__file__))
